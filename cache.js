b9d01256a853300a20ef2689d19223fce9cb44ac
luiscastro193 <luiscastro193@gmail.com>
2026-01-29 17:20:14 +0100

    Fix more edge cases

--- cache.js
+++ cache.js
@@ -1,2 +1,7 @@
 "use strict";
+navigator.serviceWorker.addEventListener('message', () => {
+	navigator.serviceWorker.controller?.postMessage(null);
+	location.reload();
+}, {once: true});
+
 navigator.serviceWorker.register(new URL('cache-sw.min.js', import.meta.url));
