e7bb4f4bc0bd7de6fa425e8df66685e087f3b0e9
Luis Castro Martín <luiscastro193@gmail.com>
2026-01-30 03:40:58 +0100

    Add safeguard

--- cache-off.js
+++ cache-off.js
@@ -1,3 +1,8 @@
 "use strict";
+navigator.serviceWorker.addEventListener('message', () => {
+	navigator.serviceWorker.controller?.postMessage(null);
+	location.reload();
+}, {once: true});
+
 const scope = new URL('.', location.href).pathname;
 navigator.serviceWorker.register(new URL('cache-off-sw.js', import.meta.url), {scope});
