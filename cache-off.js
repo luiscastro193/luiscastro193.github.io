1bacca372d0c1ba359603bbe61e93735d456282c
Luis Castro Martín <luiscastro193@gmail.com>
2026-02-03 11:17:39 +0100

    Simplify

--- cache-off.js
+++ cache-off.js
@@ -4,5 +4,5 @@ navigator.serviceWorker.addEventListener('message', () => {
 	location.reload();
 }, {once: true});
 
-const scope = new URL('.', location.href).pathname;
+const scope = new URL('.', location.href);
 navigator.serviceWorker.register(new URL('cache-off-sw.js', import.meta.url), {scope});
